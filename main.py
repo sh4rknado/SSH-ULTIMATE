756f9d91e506221d10f155cd68075cee9fb04044
Bertieaux Jordan <jordan.bertieaux@std.heh.be>
2020-07-27 19:26:40 +0200

    Add main args

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 
+import argparse
 from SSH_ENUM.SSH_ENUM import SSH_ENUM
 
 if __name__ == '__main__':
-    ssh_enum = SSH_ENUM('127.0.0.1', '/mnt/data/Hacking/Cracker/PasswordList/SecLists-master/Usernames/xato-net-10-million-usernames.txt', port=22, use_list=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", help="ip address", type=str)
+    parser.add_argument("-u", "--username", help="username or user list", type=str)
+    parser.add_argument("-p", "--port", help="port", type=int, default=22)
+    args = parser.parse_args()
+
+    ip = None
+    username = None
+    port = args.port
+
+    if args.ip:
+        ip = args.ip
+    if args.username:
+        username = args.username
+
+    ssh_enum = SSH_ENUM(ip, username, port=port, use_list=True)
     ssh_enum.run()
